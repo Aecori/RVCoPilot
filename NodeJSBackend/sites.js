414b01bf23c128233c10b0a82605434af86891a5
Patrick Godfrey <godfrepa@oregonstate.edu>
2024-04-24 21:17:30 -0400

    Added bodyparser

--- NodeJSBackend/sites.js
+++ NodeJSBackend/sites.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+router.use(bodyParser.json());
 const ds = require('./datastore');
 const datastore = ds.datastore;
 const math = require('mathjs');
